ca595aae762b7c65c6895127f5be5ee2c8f28358
IraMira333 <irinat333@gmail.com>
2024-01-16 17:56:57 +0200

    Added component Home

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+const HomePart = () => {
+  return (
+    <>
+      <h1>Antonii Kurts</h1>
+      <Image src="/vercel.svg" alt="Vercel Logo" width={100} height={24} />
+    </>
+  );
+};
+export default HomePart;
