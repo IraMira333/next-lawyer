3b9e5e8c001493889bbf4edb0e2cbcb7cadde360
IraMira333 <irinat333@gmail.com>
2024-01-25 22:58:52 +0200

    Added 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function NotFound() {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginTop: "20px",
+      }}
+    >
+      <Image src="/404NotFound.png" alt="NotFound" width={300} height={300} />
+    </div>
+  );
+}
