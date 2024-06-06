53a03dec240967c86a2cd9e966df1c887e24f2c2
IraMira333 <irinat333@gmail.com>
2024-06-06 22:00:17 +0300

    Update ServicesList.tsx

--- src/components/ServicesComponents/ServicesList.tsx
+++ src/components/ServicesComponents/ServicesList.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
+import { SquareSmall } from "../shared/SquareSmall";
+import { useTranslations } from "next-intl";
+import { SquareBig } from "../shared/SquareBig";
 
 export const ServicesList = () => {
+  const t = useTranslations("Home");
+
   return (
     <section className="px-4 py-10 tab:px-5 pc:px-9 pc:pt-[100px]">
       <h2 className="font-lora text-accent text-center mb-5 pc:mb-10 text-[28px] leading-8 tab:text-3xl pc:text-[40px] pc:leading-[48px] font-medium uppercase">
@@ -19,6 +24,62 @@ export const ServicesList = () => {
         <span className="font-lora text-accent text-[26px] leading-8 ml-auto">
           01
         </span>
+      </div>
+      <div>
+        <h3 className="text-base leading-[21px] tab:text-lg font-semibold my-4">
+          {t("whomBox1Title")}
+        </h3>
+        <ul className="mb-4 tab:text-base tab:leading-[21px]">
+          <li className="flex gap-2 text-start">
+            <div className="mt-2">
+              <SquareSmall />
+            </div>
+            <p>{t("whomBox1Li1")}</p>
+          </li>
+          <li className="flex gap-2 text-start">
+            <div className="mt-2">
+              <SquareSmall />
+            </div>
+            <p>{t("whomBox1Li2")}</p>
+          </li>
+          <li className="flex gap-2 text-start">
+            <div className="mt-2">
+              <SquareSmall />
+            </div>
+            <p>{t("whomBox1Li3")}</p>
+          </li>
+          <li className="flex gap-2 text-start">
+            <div className="mt-2">
+              <SquareSmall />
+            </div>
+            <p>{t("whomBox1Li4")}</p>
+          </li>
+          <li className="flex gap-2 text-start">
+            <div className="mt-2">
+              <SquareSmall />
+            </div>
+            <p>{t("whomBox1Li5")}</p>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p>
+          На протязі років, в галузі державного управління змінилось багато що,
+          але явища не ефективності чиновницьких рішень і бюрократії, виконання
+          безглуздих вказівок керівництва  є сталими.
+        </p>
+        <p>
+          Як правило, керівники державних територіальних органів і підрозділів
+          міграційної служби України не розуміють викладені у заявах
+          аргументовані  доводи та пояснення. Їм байдуже щодо тяжкості
+          наслідків, що настають для окремої людини за результатом прийнятих
+          ними необгрунтованих рішень. Єдиним доводом у суперечці  було та
+          залишається рішення суду, яким встановлено їх протиправну поведінку з
+          перспективою подальшої персональної відповідальності.
+        </p>
+      </div>
+      <div className="flex justify-end tab:items-end ">
+        <SquareBig />
       </div>
       <div className="max-w-[338px] mx-auto rounded overflow-hidden tab:ml-0 pc:min-w-[371px] ">
         <Image
