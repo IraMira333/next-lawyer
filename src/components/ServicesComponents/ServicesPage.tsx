8ac9619c0d2ace13b45fba01accc02135123ac98
IraMira333 <irinat333@gmail.com>
2024-06-06 16:59:56 +0300

    Create ServicesPage.tsx

---
+++ src/components/ServicesComponents/ServicesPage.tsx
@@ -0,0 +1,11 @@
+import { HeroServices } from "./HeroServices";
+import { ServicesList } from "./ServicesList";
+
+export const ServicesPage = () => {
+  return (
+    <>
+      <HeroServices />
+      <ServicesList />
+    </>
+  );
+};
