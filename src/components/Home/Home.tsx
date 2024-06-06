b13ac817f1dcc600688762193c9d7e12712f98d8
IraMira333 <irinat333@gmail.com>
2024-06-06 13:04:50 +0300

    Update Home.tsx

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { useLocale, useTranslations } from "next-intl";
 import { SquareSmall } from "../shared/SquareSmall";
 import { ButtonDetails } from "../shared/ButtonDetails";
+import { SquareBig } from "../shared/squareBig";
 
 const HomePart = () => {
   const t = useTranslations("Home");
@@ -146,78 +147,92 @@ const HomePart = () => {
           </li>
         </ul>
       </section>
-      <section className="bg-accentBg pt-8 px-3 tab:px-5 pb-[60px] shadow-custom">
-        <div className="max-w-[500px] tab:max-w-[1000px] ml-auto mr-auto">
-          <h2 className="font-lora text-accent text-center mb-5 text-lg tab:text-xl font-medium">
+      <section className="bg-accentBg py-10 px-4 tab:px-5 tab:py-[60px]">
+        <div className="max-w-[500px] tab:max-w-[900px] ml-auto mr-auto">
+          <h2 className="font-lora text-accent text-center mb-5 text-[28px] leading-8 tab:text-xl font-medium">
             {t("whomOtherServicesTitle")}
           </h2>
-          <ul>
+          <ul className="mb-[22px]">
             <li>
-              <Image
-                src="https://drive.google.com/uc?export=view&id=16EMZ2DM4nXKKzhffY1R5ndEgY0Uqmuyz"
-                alt="picture"
-                width={218}
-                height={212}
-                className="flex object-cover mx-auto tab:min-w-[350px] tab:max-h-[auto]"
-              />
-              <h3 className="font-robotoSlab text-lg text-center mt-3">
+              <h3 className="text-base leading-[21px] font-semibold">
                 {t("whomBox4Title1")}
               </h3>
+              <div className="h-[2px] w-full bg-accent -mt-1"></div>
 
-              <ul>
-                <li>
-                  <h4 className="font-robotoSlab text-base text-start my-2 tab:ml-4">
-                    {t("whomBox4Li1")}
-                  </h4>
-                  <p className="text-justify bigmob:leading-6">
-                    {t("whomBox4Li1P1")}
-                  </p>
+              <ul className="mt-[23px] mb-6">
+                <li className="mb-4">
+                  <div className="flex gap-2 text-start mb-2">
+                    <div className="mt-2">
+                      <SquareSmall />
+                    </div>
+                    <h4 className="text-base font-semibold text-start tab:ml-4">
+                      {t("whomBox4Li1")}
+                    </h4>
+                  </div>
+                  <p className=" leading-[18px]">{t("whomBox4Li1P1")}</p>
                 </li>
                 <li>
-                  <h4 className="font-robotoSlab text-base text-start my-2 tab:ml-4">
-                    {t("whomBox4Li2")}
-                  </h4>
-                  <p className="text-justify bigmob:leading-6">
-                    {t("whomBox4Li2P1")}
-                  </p>
+                  <div className="flex gap-2 text-start mb-2">
+                    <div className="mt-2">
+                      <SquareSmall />
+                    </div>
+                    <h4 className="text-base font-semibold text-start tab:ml-4">
+                      {t("whomBox4Li2")}
+                    </h4>
+                  </div>
+                  <p className=" leading-[18px]">{t("whomBox4Li2P1")}</p>
                 </li>
               </ul>
             </li>
             <li>
-              <h3 className="font-robotoSlab text-lg text-center my-3">
+              <h3 className="text-base leading-[21px] font-semibold">
                 {t("whomBox4Title2")}
               </h3>
-              <p className="text-justify bigmob:leading-6">
-                {t("whomBox4Li3")}
-              </p>
+              <div className="h-[2px] w-full bg-accent -mt-1"></div>
+
+              <p className=" leading-[18px] mt-3">{t("whomBox4Li3")}</p>
             </li>
           </ul>
-        </div>
-      </section>
-      <section className="px-3 pt-6 pb-[60px]">
-        <div className="max-w-[500px] ml-auto mr-auto">
-          <div>
-            <div className="max-w-[384px] ml-auto">
-              <h2 className="font-robotoSlab text-base font-medium text-accent text-right mb-4 ">
-                {t("whyTitle")}
-              </h2>
-              <h3 className="font-lora italic text-xl mb-5">{t("whyP4")}</h3>
-            </div>
+          <div className="max-w-[380px] mx-auto tab:mx-0 tab:min-w-[356px] tab:max-w-[46%] rounded overflow-hidden">
             <Image
-              src="/ak-second-photo.jpeg"
-              alt="Lawyer Photo"
-              width={213}
-              height={142}
-              // width={426}
-              // height={284}
-              className=""
+              src="https://drive.google.com/uc?export=view&id=16EMZ2DM4nXKKzhffY1R5ndEgY0Uqmuyz"
+              alt="picture"
+              width={2000}
+              height={1350}
+              className="aspect-[288/173] tab:aspect-[356/214] pc:aspect-[593/266] flex object-cover "
             />
           </div>
-          <div>
-            <div className="mt-3 text-justify tab:text-base font-light">
+        </div>
+      </section>
+      <section className="px-4 tab:px-5 py-10 tab:py-[60px]">
+        <div className="max-w-[500px] tab:max-w-[900px] mx-auto ">
+          <div className="tab:flex tab:justify-between">
+            <div className="max-w-[384px] tab:max-w-[57%] mx-auto tab:mx-0">
+              <h2 className="font-lora text-2xl tab:text-3xl font-medium text-accent text-center tab:text-left mb-4 uppercase">
+                {t("whyTitle")}
+              </h2>
+              <h3 className="font-lora italic text-lg leading-[26px] mb-4 uppercase">
+                {t("whyP4")}
+              </h3>
+            </div>
+            <div className="max-w-[380px] mx-auto tab:mx-0 tab:min-w-[270px] tab:max-w-[34%] rounded overflow-hidden mb-4">
+              <Image
+                src="https://drive.google.com/uc?export=view&id=1GmIOtH5AhdDSIgpi71_tFZOQXOEO72wE"
+                alt="Lawyer Photo"
+                width={1280}
+                height={854}
+                className="aspect-[288/193] tab:aspect-[263/176] pc:aspect-[234/156] flex object-cover "
+              />
+            </div>
+          </div>
+          <div className="tab:flex tab:justify-between">
+            <div className="tab:text-base leading-[18px] tab:min-w-[502px] tab:w-[75%]">
               <p className="indent-6">{t("whyP1")}</p>
               <p className="indent-6">{t("whyP2")}</p>
               <p className="indent-6">{t("whyP3")}</p>
+            </div>
+            <div className="flex justify-end tab:items-end ">
+              <SquareBig />
             </div>
           </div>
         </div>
