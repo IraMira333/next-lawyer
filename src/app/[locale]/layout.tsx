842208d8386456b49de3e7c08a6f95d89e71bbf6
IraMira333 <irinat333@gmail.com>
2024-06-08 01:24:39 +0300

    Update layout.tsx

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -73,11 +73,7 @@ export default function LocaleLayout({
     >
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <meta
-          name="image"
-          property="og:image"
-          content="/imageForSharing.jpeg"
-        />
+        <meta name="image" property="og:image" content="/imageForSharing.jpg" />
       </head>
       <NextIntlClientProvider locale={locale} messages={messages}>
         <body>
