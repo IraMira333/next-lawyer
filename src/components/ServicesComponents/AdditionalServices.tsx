db0473869b9ba845708513aacbe6cae34660fb1a
IraMira333 <irinat333@gmail.com>
2024-06-08 00:55:38 +0300

    Update AdditionalServices.tsx

--- src/components/ServicesComponents/AdditionalServices.tsx
+++ src/components/ServicesComponents/AdditionalServices.tsx
@@ -2,20 +2,20 @@ import { SquareSmall } from "../shared/SquareSmall";
 
 export const AdditionalServices = () => {
   return (
-    <section className="bg-accentBg px-4 py-10 tab:px-5 pc:px-9 pc:pt-[100px]">
-      <h2 className="font-lora text-accent text-center mb-5 pc:mb-10 text-[28px] leading-8 tab:text-3xl pc:text-[40px] pc:leading-[48px] font-medium uppercase">
+    <section className="bg-accentBg px-4 py-10 tab:px-5 pc:px-[38px] pc:py-[60px]">
+      <h2 className="font-lora text-accent text-center mb-5 pc:mb-4 text-[28px] leading-8 tab:text-3xl pc:text-[40px] pc:leading-[48px] font-medium uppercase">
         Додаткові послуги
       </h2>
-      <p className="leading-[18px] text-center mb-5">
+      <p className="leading-[18px] text-center mb-5 tab:text-base pc:mb-10">
         Також ви можете звернутись до нас, якщо вам необхідно отримати:
       </p>
-      <ul className="min-w-[288px] max-w-[500px] mx-auto tab:max-w-[900px] pc:max-w-[1800px]">
-        <li className="tab:relative pc:flex pc:gap-4 border border-accent rounded-lg overflow-hidden p-4 mb-4 pc:p-5 pc:mb-5">
+      <ul className="min-w-[288px] max-w-[500px] mx-auto tab:max-w-[900px] tab:flex tab:flex-wrap tab:gap-4 justify-center pc:max-w-[1800px]">
+        <li className="tab:relative tab:w-tabAdditionalServices pc:w-pcAdditionalServices border border-accent rounded-lg overflow-hidden p-4 mb-4 tab:mb-0 pc:p-5 pc:mb-5">
           <h3 className="text-base leading-[21px] tab:text-lg font-semibold">
             Консультацію чи повний правовий супровід з питань імміграції та
             проживання
           </h3>
-          <div className="h-[2px] w-full bg-accent -mt-1 tab:w-[90%] "></div>
+          <div className="h-[2px] w-full bg-accent "></div>
           <ul className="tab:text-base tab:leading-[21px]  mt-4">
             <li className="flex gap-2 text-start mb-2">
               <div className="mt-2">
@@ -40,12 +40,12 @@ export const AdditionalServices = () => {
             </li>
           </ul>
         </li>
-        <li className="tab:relative pc:flex pc:gap-4 border border-accent rounded-lg overflow-hidden p-4 mb-4 pc:p-5 pc:mb-5">
+        <li className="tab:relative tab:w-tabAdditionalServices pc:w-pcAdditionalServices border border-accent rounded-lg overflow-hidden p-4 mb-4 tab:mb-0 pc:p-5 pc:mb-5">
           <h3 className="text-base leading-[21px] tab:text-lg font-semibold">
             Консультацію чи повний правовий супровід з питань громадянства
             України
           </h3>
-          <div className="h-[2px] w-full bg-accent -mt-1 tab:w-[90%] "></div>
+          <div className="h-[2px] w-full bg-accent "></div>
           <ul className="tab:text-base tab:leading-[21px]  mt-4">
             <li className="flex gap-2 text-start mb-2">
               <div className="mt-2">
@@ -65,12 +65,12 @@ export const AdditionalServices = () => {
             </li>
           </ul>
         </li>
-        <li className="tab:relative pc:flex pc:gap-4 border border-accent rounded-lg overflow-hidden p-4 mb-4 pc:p-5 pc:mb-5">
+        <li className="tab:relative tab:w-tabAdditionalServices pc:w-pcAdditionalServices border border-accent rounded-lg overflow-hidden p-4 mb-4 tab:mb-0 pc:p-5 pc:mb-5">
           <h3 className="text-base leading-[21px] tab:text-lg font-semibold">
             Консультацію чи правову допомогу у зв'язку з прийняттям рішення
             припинити громадянство України
           </h3>
-          <div className="h-[2px] w-full bg-accent -mt-1 tab:w-[90%] "></div>
+          <div className="h-[2px] w-full bg-accent "></div>
           <ul className="tab:text-base tab:leading-[21px]  mt-4">
             <li className="flex gap-2 text-start mb-2">
               <div className="mt-2">
