9750134aa6cabb0f5ee858237732eed9c595ee8e
IraMira333 <irinat333@gmail.com>
2024-06-17 23:20:54 +0300

    Added smoothness for Button

--- src/components/shared/ButtonDetails.tsx
+++ src/components/shared/ButtonDetails.tsx
@@ -7,7 +7,7 @@ export const ButtonDetails = ({ url, text }: { url: string; text: string }) => {
         aria-label="details button"
         type="button"
         className="flex gap-[13px] font-lora text-lg leading-6 px-4 py-2 border border-nav rounded items-center
-        hover:bg-accentGradient hover:border-accent hover:text-white group"
+        hover:bg-accentGradient hover:border-accent hover:text-white group transition-all duration-[250ms] ease-[cubic-bezier(0.4,0,0.2,1)]"
       >
         {text} <IconArrow className="text-accent group-hover:text-white" />
       </button>
