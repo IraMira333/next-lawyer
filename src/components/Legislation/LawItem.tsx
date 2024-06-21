4a6588c90fd17538b14ed712a042aba8d929feff
IraMira333 <irinat333@gmail.com>
2024-06-22 00:50:44 +0300

    Update LawItem.tsx

--- src/components/Legislation/LawItem.tsx
+++ src/components/Legislation/LawItem.tsx
@@ -1,5 +1,4 @@
 import { legislationType } from "@/mockedData/legislation";
-import Link from "next/link";
 import { SquareSmall } from "../shared/SquareSmall";
 
 export const LawItem = ({ item }: { item: legislationType }) => {
@@ -8,7 +7,14 @@ export const LawItem = ({ item }: { item: legislationType }) => {
       <div className="mt-2">
         <SquareSmall />
       </div>
-      <Link href={item.link}></Link>;
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        className="underline hover:text-accent focus:text-accent active:text-accent transition-all duration-[250ms] ease-[cubic-bezier(0.4,0,0.2,1)]"
+        href={item.link}
+      >
+        {item.legislationTitle}
+      </a>
     </>
   );
 };
