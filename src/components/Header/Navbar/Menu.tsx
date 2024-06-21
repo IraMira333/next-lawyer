cc70ea247aa108d91270158e72eef480a0511d61
IraMira333 <irinat333@gmail.com>
2024-06-22 00:50:41 +0300

    Update Menu.tsx

--- src/components/Header/Navbar/Menu.tsx
+++ src/components/Header/Navbar/Menu.tsx
@@ -5,6 +5,8 @@ import { AiOutlineSend } from "react-icons/ai";
 const Menu = ({ className }: { className: string }) => {
   const locale = useLocale();
   const t = useTranslations("Menu");
+  const navTextStyle =
+    "font-lora font-medium tab:text-base pc:text-lg cursor-pointer hover:text-accent focus:text-accent active:text-accent transition-all duration-[250ms] ease-[cubic-bezier(0.4,0,0.2,1)] uppercase";
 
   return (
     <>
@@ -13,7 +15,7 @@ const Menu = ({ className }: { className: string }) => {
           <AiOutlineSend size={12} color="orange" />
         </div>
         <Link
-          className={`font-lora font-medium tab:text-base pc:text-lg cursor-pointer hover:text-accent active:text-accent transition-all duration-[250ms] ease-[cubic-bezier(0.4,0,0.2,1)] uppercase ${className}`}
+          className={`${navTextStyle} ${className}`}
           href={`/${locale}/services`}
         >
           {t("services")}
@@ -24,7 +26,7 @@ const Menu = ({ className }: { className: string }) => {
           <AiOutlineSend size={12} color="orange" />
         </div>
         <Link
-          className={`font-lora font-medium tab:text-base pc:text-lg cursor-pointer hover:text-accent active:text-accent transition-all duration-[250ms] ease-[cubic-bezier(0.4,0,0.2,1)] uppercase ${className}`}
+          className={`${navTextStyle} ${className}`}
           href={`/${locale}/legislation`}
         >
           {t("legislation")}
@@ -36,7 +38,7 @@ const Menu = ({ className }: { className: string }) => {
           <AiOutlineSend size={12} color="orange" />
         </div>
         <Link
-          className={`font-lora font-medium tab:text-base pc:text-lg cursor-pointer hover:text-accent active:text-accent transition-all duration-[250ms] ease-[cubic-bezier(0.4,0,0.2,1)] uppercase ${className}`}
+          className={`${navTextStyle} ${className}`}
           href={`/${locale}/contacts`}
         >
           {t("contacts")}
