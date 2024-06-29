d148d234fd05b3d7e8537645a5f28b4af4f40beb
IraMira333 <irinat333@gmail.com>
2024-06-29 23:09:15 +0300

    Updated styles

--- src/components/Legislation/LegislationList.tsx
+++ src/components/Legislation/LegislationList.tsx
@@ -71,9 +71,9 @@ export const LegislationList = () => {
         );
 
   return (
-    <section className="px-4 py-10 tab:px-5 tab:py-[60px] pc:px-10 pc:pt-[100px]">
-      <div className="min-w-[288px] max-w-[600px] mx-auto tab:max-w-[900px] pc:max-w-[1800px]">
-        <div className="tab:flex gap-4 mb-4 justify-center items-center">
+    <section className="px-4 py-10 tab:px-5 tab:py-[60px] pc:px-10 pc:pt-[100px] tab:text-base">
+      <div className="min-w-[288px] max-w-[600px] mx-auto tab:max-w-[700px] pc:max-w-[900px]">
+        <div className="tab:flex gap-4 mb-5 pc:mb-7 justify-center items-center">
           <h3
             id="category"
             className="text-base leading-[21px] tab:text-lg font-semibold mb-4 tab:mb-0"
