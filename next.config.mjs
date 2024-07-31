89c9551c59abd508a1c0305419f8b74db823049c
IraMira333 <irinat333@gmail.com>
2024-08-01 00:29:32 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -20,8 +20,7 @@ const nextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value:
-              "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; connect-src 'self'; font-src 'self'; frame-src 'self'",
+            value: `script-src 'self' https://maps.googleapis.com https://maps.gstatic.com 'unsafe-inline' 'unsafe-eval'`,
           },
           {
             key: "X-Frame-Options",
