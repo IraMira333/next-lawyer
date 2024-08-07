9d1061a530d985ac127976f43522bc9f1428752c
IraMira333 <irinat333@gmail.com>
2024-08-07 23:46:34 +0300

    Update i18n.ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,8 +1,6 @@
 import { notFound } from "next/navigation";
 import { getRequestConfig } from "next-intl/server";
-
-// Can be imported from a shared config
-export const locales = ["en", "ru", "ua"];
+import { locales } from "./config";
 
 export default getRequestConfig(async ({ locale }: { locale: string }) => {
   // Validate that the incoming `locale` parameter is valid
