e38cc481a62af476c9d6c0f39aaa840e7eaa16c4
IraMira333 <irinat333@gmail.com>
2025-06-15 00:33:40 +0300

    Update InterpolIs.tsx

--- src/components/Interpol/InterpolIs.tsx
+++ src/components/Interpol/InterpolIs.tsx
@@ -1,5 +1,6 @@
 import { useTranslations } from "next-intl";
 import { Color, SquareSmall } from "../shared/SquareSmall";
+import AccessibilitySection from "../Home/AccessibilitySection";
 
 export const InterpolIs = () => {
   const t = useTranslations("Interpol");
@@ -11,7 +12,6 @@ export const InterpolIs = () => {
         indent: (chunk) => <span className="block indent-7">{chunk}</span>,
       }),
     },
-    { title: t("title3"), text: t("text3") },
   ];
   interface Notice {
     color: Color;
@@ -28,54 +28,50 @@ export const InterpolIs = () => {
     { text: "list1Item8", color: "grey" },
   ];
   return (
-    <section className="py-10 px-4 tab:px-5 tab:py-[60px] pc:px-9">
-      <div className="max-w-[500px] tab:max-w-[900px] pc:max-w-[1800px] ml-auto mr-auto">
-        <div className="">
-          {infoList.map((item, index) => (
-            <div key={index} className="">
-              <h3 className="text-lg tab:text-2xl font-semibold">
-                {item.title}
-              </h3>
-              <p className="mt-1 tab:text-lg mb-6 text-justify indent-7">
-                {item.text}
-              </p>
-            </div>
-          ))}
-          <div className="">
-            <h3 className="text-lg tab:text-2xl font-semibold">
-              {t("titleList1")}
-            </h3>
-            <div className="h-[2px] w-full bg-accent  "></div>
-
-            <ul className="mt-[23px] mb-6 tab:mb-0 grid grid-cols-1 tab:grid-cols-2 gap-x-6">
-              {notices.map((item) => (
-                <li key={item.color} className="mb-4">
-                  <div className="flex gap-2 text-start mb-2">
-                    <div className="mt-1">
-                      <SquareSmall color={item.color} size />
-                    </div>
-                    <p className="leading-[18px] tab:text-base">
-                      {t.rich(item.text, {
-                        bold: (chunk) => (
-                          <span className="text-base font-semibold text-start tab:ml-4">
-                            {chunk}
-                          </span>
-                        ),
-                        br: () => <br />,
-                      })}
-                    </p>
-                  </div>
-                </li>
-              ))}
-            </ul>
-
-            {/* <div className="flex justify-center tab:justify-end tab:mt-[50px] pc:mt-[80px]">
-              <ButtonDetails
-                text={t("detailsButton")}
-                url={`/${locale}/services#termination`}
-              />
-            </div> */}
+    <section className="">
+      <div className="pt-10 px-4 tab:px-5 tab:pt-[60px] pc:px-9 max-w-[500px] tab:max-w-[900px] pc:max-w-[1800px] ml-auto mr-auto">
+        {infoList.map((item, index) => (
+          <div key={index} className="">
+            <h3 className="text-lg tab:text-2xl font-semibold">{item.title}</h3>
+            <p className="mt-1 tab:text-lg mb-6 text-justify indent-7">
+              {item.text}
+            </p>
           </div>
+        ))}
+      </div>
+      <AccessibilitySection />
+      <div className="pb-10 pt-6 px-4 tab:px-5 tab:pb-[60px] pc:px-9 max-w-[500px] tab:max-w-[900px] pc:max-w-[1800px] ml-auto mr-auto">
+        <h3 className="text-lg tab:text-2xl font-semibold">{t("title3")}</h3>
+        <p className="mt-1 tab:text-lg mb-6 text-justify indent-7">
+          {t("text3")}
+        </p>
+        <div className="">
+          <h3 className="text-lg tab:text-2xl font-semibold">
+            {t("titleList1")}
+          </h3>
+          <div className="h-[2px] w-full bg-accent  "></div>
+
+          <ul className="mt-[23px] mb-6 tab:mb-0 grid grid-cols-1 tab:grid-cols-2 gap-x-6">
+            {notices.map((item) => (
+              <li key={item.color} className="mb-4">
+                <div className="flex gap-2 text-start mb-2">
+                  <div className="mt-1">
+                    <SquareSmall color={item.color} size />
+                  </div>
+                  <p className="leading-[18px] tab:text-base">
+                    {t.rich(item.text, {
+                      bold: (chunk) => (
+                        <span className="text-base font-semibold text-start tab:ml-4">
+                          {chunk}
+                        </span>
+                      ),
+                      br: () => <br />,
+                    })}
+                  </p>
+                </div>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </section>
