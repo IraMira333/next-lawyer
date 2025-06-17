d2cc06275c24a8aaacb65422590c7ae4e19cae6e
IraMira333 <irinat333@gmail.com>
2025-06-17 14:45:39 +0300

    Changed font

--- src/components/Modal/CustomCheckbox.tsx
+++ src/components/Modal/CustomCheckbox.tsx
@@ -14,7 +14,7 @@ export const CustomCheckbox = ({
           type="checkbox"
           checked={checked}
           onChange={onChange}
-          className={`hidden peer ${className}`}
+          className={`hidden peer font-lora ${className}`}
         />
         <FaCheck className="peer-checked:text-white text-transparent min-w-5 h-5 border-2 border-gray-400 peer-checked:border-green-500 peer-checked:bg-green-500 transition-colors flex items-center justify-center mt-1" />
         <span className="ml-2 text-white text-base">{label}</span>
