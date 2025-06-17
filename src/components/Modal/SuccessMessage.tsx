f9b4789172e12bac274c6b3bfc8d6b0b1ebdc5f5
IraMira333 <irinat333@gmail.com>
2025-06-17 14:47:40 +0300

    Updated mt

--- src/components/Modal/SuccessMessage.tsx
+++ src/components/Modal/SuccessMessage.tsx
@@ -6,7 +6,7 @@ export const SuccessMessage = () => {
 
   return (
     <div className="relative z-[-1] overflow-hidden w-screen h-screen">
-      <div className="min-w-[288px] w-[80%] max-w-[436px] pc:max-w-[485px] mx-auto mt-[40vh] tab:mt-[50vh] text-center text-white">
+      <div className="min-w-[288px] w-[80%] max-w-[436px] pc:max-w-[485px] mx-auto mt-[30vh] tab:mt-[40vh] text-center text-white">
         <h2 className="font-semibold text-xl tab:text-4xl uppercase mb-5">
           {t("successTitle")}
         </h2>
