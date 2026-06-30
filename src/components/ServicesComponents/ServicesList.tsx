e990f941b3fe97457746e1b981e027a14e417365
IraMira333 <irinat333@gmail.com>
2026-06-30 22:48:59 +0300

    Created CTA Request

--- src/components/ServicesComponents/ServicesList.tsx
+++ src/components/ServicesComponents/ServicesList.tsx
@@ -2,17 +2,18 @@ import Image from "next/image";
 import { useTranslations } from "next-intl";
 import { SquareSmall } from "../shared/SquareSmall";
 import { SquareBig } from "../shared/SquareBig";
+import { IconPhone } from "../icons/IconPhone";
 
 export const ServicesList = () => {
   const t = useTranslations("Services");
 
   return (
-    <section className="px-4 py-10 tab:px-5 tab:py-[60px] pc:px-10 pc:pt-[100px]">
-      <h2 className="font-lora text-accent text-center mb-5 pc:mb-10 text-[28px] leading-8 tab:text-3xl pc:text-[40px] pc:leading-[48px] font-medium uppercase">
+    <section className=" py-10  tab:py-[60px]  pc:pt-[100px]">
+      <h2 className="font-lora px-4 tab:px-5 pc:px-10 text-accent text-center mb-5 pc:mb-10 text-[28px] leading-8 tab:text-3xl pc:text-[40px] pc:leading-[48px] font-medium uppercase">
         {t("titleServicesList")}
       </h2>
-      <ul className="min-w-[288px] max-w-[500px] mx-auto tab:max-w-[900px] pc:max-w-[1800px]">
-        <li className="tab:flex tab:gap-4 pc:gap-[62px] tab:mb-10 pc:mb-[60px] tab:relative">
+      <ul className="">
+        <li className="tab:flex min-w-[288px] max-w-[500px] mx-auto tab:max-w-[900px] pc:max-w-[1800px] tab:gap-4 pc:gap-[62px] tab:mb-10 px-4 tab:px-5 pc:px-10 pc:mb-[60px] tab:relative">
           <div
             id="solutions01"
             className="max-w-[338px] mx-auto rounded tab:rounded-none overflow-hidden tab:ml-0 
@@ -89,12 +90,35 @@ export const ServicesList = () => {
                 01
               </span>
             </div>
-            <div className="flex justify-end tab:absolute tab:right-0 bottom-0 mb-10 tab:m-0">
+            <div className="flex justify-end tab:absolute tab:right-5 pc:right-10 bottom-0 mb-10 tab:m-0">
               <SquareBig />
             </div>
           </div>
         </li>
-        <li className="tab:relative tab:flex tab:flex-row-reverse tab:gap-4 pc:gap-[62px] tab:mb-10 pc:mb-[60px]">
+        <li className="bg-accentBg shadow-inner shadow-accentBgDark py-10 px-4 tab:px-5 mb-10 pc:mb-[60px] tab:py-[60px] pc:px-9 ">
+          <div className="max-w-[500px] tab:max-w-[900px] pc:max-w-[1800px] ml-auto mr-auto tab:flex tab:justify-between tab:items-center tab:gap-6 pc:gap-32">
+            <div className="hidden tab:block tab:w-[18%] pc:w-[13%] desk:w-[10%]">
+              <Image src="/vec1.png" alt="decor icon" width={300} height={300} />
+            </div>
+            <div className="tab:w-[60%] pc:w-[50%]">
+              <h2 className="font-lora text-accent text-xl tab:text-2xl italic font-semibold text-center tab:text-left uppercase mb-4">
+                {t("lawRequest")}
+              </h2>
+              <p className="text-lg whitespace-pre-line text-center tab:text-left mb-4">
+                {t("lawRequestText")}
+              </p>
+              <div className="flex gap-2 mb-3 justify-center tab:justify-normal pc:mb-4 hover:text-accent active:text-accent transition-all duration-[250ms] ease-[cubic-bezier(0.4,0,0.2,1)]">
+                <div className="w-6 h-6 flex justify-center items-center">
+                  <IconPhone className=" " />
+                </div>
+                <a href="tel:+380930324874" className="font-lora">
+                  +38 (093) 032-48-74
+                </a>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li className="tab:relative min-w-[288px] max-w-[500px] mx-auto tab:max-w-[900px] pc:max-w-[1800px] px-4 tab:px-5 pc:px-10 tab:flex tab:flex-row-reverse tab:gap-4 pc:gap-[62px] tab:mb-10 pc:mb-[60px]">
           <div
             id="solutions02"
             className="max-w-[338px] mx-auto rounded tab:rounded-none overflow-hidden 
@@ -179,12 +203,12 @@ export const ServicesList = () => {
                 02
               </span>
             </div>
-            <div className="flex justify-end tab:absolute tab:right-0 bottom-0 mb-10 tab:m-0">
+            <div className="flex justify-end tab:absolute tab:right-5 pc:right-10 bottom-0 mb-10 tab:m-0">
               <SquareBig />
             </div>
           </div>
         </li>
-        <li className="tab:relative tab:flex tab:gap-4 pc:gap-[62px] tab:mb-10 pc:mb-[60px]">
+        <li className="tab:relative min-w-[288px] max-w-[500px] mx-auto tab:max-w-[900px] pc:max-w-[1800px] px-4 tab:px-5 pc:px-10 tab:flex tab:gap-4 pc:gap-[62px] tab:mb-10 pc:mb-[60px]">
           <div
             id="solutions03"
             className="max-w-[338px] mx-auto pc:mx-0 rounded tab:rounded-none overflow-hidden 
@@ -254,12 +278,12 @@ export const ServicesList = () => {
                 03
               </span>
             </div>
-            <div className="flex justify-end tab:absolute tab:right-0 bottom-0 mb-10 tab:m-0">
+            <div className="flex justify-end  tab:absolute tab:right-5 pc:right-10 bottom-0 mb-10 tab:m-0">
               <SquareBig />
             </div>
           </div>
         </li>
-        <li className="tab:relative tab:flex tab:flex-row-reverse tab:gap-4 pc:gap-[62px] tab:mb-10 pc:mb-[60px]">
+        <li className="tab:relative min-w-[288px] max-w-[500px] mx-auto tab:max-w-[900px] pc:max-w-[1800px] px-4 tab:px-5 pc:px-10 tab:flex tab:flex-row-reverse tab:gap-4 pc:gap-[62px] tab:mb-10 pc:mb-[60px]">
           <div
             id="solutions04"
             className="max-w-[338px] mx-auto rounded tab:rounded-none overflow-hidden 
@@ -326,7 +350,7 @@ export const ServicesList = () => {
                 04
               </span>
             </div>
-            <div className="flex justify-end tab:absolute tab:right-0 bottom-0 mb-10 tab:m-0">
+            <div className="flex justify-end tab:absolute tab:right-5 pc:right-10 bottom-0 mb-10 tab:m-0">
               <SquareBig />
             </div>
           </div>
